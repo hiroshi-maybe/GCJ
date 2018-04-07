5ec9f81039e448992df661e9269a051d77fd1dbb
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-04-07 11:29:34 -0700

    submit SavingTheUniverseAgain

---
+++ 2018-QR/SavingTheUniverseAgain.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+int f(string &P) {
+  int res=0,x=1;
+  REP(i,SZ(P)) {
+    if(P[i]=='C') x*=2;
+    else res+=x;
+  }
+  return res;
+}
+
+string solve(int D, string P) {
+  if(count(P.begin(),P.end(),'S')>D) return "IMPOSSIBLE";
+  int res=0,N=SZ(P);
+  while(f(P)>D) {
+    for(int i=N-1; i>0; --i) if(P[i]=='S'&&P[i-1]=='C') {
+      swap(P[i],P[i-1]);
+      ++res;
+      break;
+    }
+  }
+//  dump2(P,f(P));
+  return to_string(res);
+}
+
+// 10:40-11:20
+int main() {
+  int T;
+  cin >> T;
+  for(int t = 1; t <= T; ++t) {
+    int D; string P;
+    cin >> D >> P;
+    cout << "Case #" << t << ": " << solve(D,P) << endl;
+  }
+  return 0;
+}
