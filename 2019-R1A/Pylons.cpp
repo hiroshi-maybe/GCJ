93abc455c353c0ddb7e0c8c23afeaa1a4e6f33e1
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-04-14 23:22:09 -0700

    add solution of GCJ 2019 R1A A. Pylons large test set

--- 2019-R1A/Pylons.cpp
+++ 2019-R1A/Pylons.cpp
@@ -31,7 +31,7 @@ template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; +
 void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
 template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
 template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
-//#define __KUMATRACE__ true
+#define __KUMATRACE__ true
 #ifdef __KUMATRACE__
 #define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
 #define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
@@ -42,25 +42,138 @@ template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, con
 #define dumpC(beg,end)
 #endif
 
-// $ cp-batch Pylons | diff Pylons.out -
 // $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address Pylons.cpp && ./a.out < Pylons.in | diff Pylons.out -
 
 /*
  
  4/12/2019
  
- 18:52-19:58 small passed
+ 18:52-19:58 small test set passed
+
+ Generally this is Hamiltonian path problem which is NP-hard.
+ We need some idea unique to this problem.
+
+ I analyzed impossible cases to construct a rule.
+ I figured out that 2xa(a<=3), 3xb(b<=3). However I couldn't move forward.
+ 
+ 4/14/2019
+ 
+ 19:15-19:55 read editorials and large test case passed by random shuffle.
+ 22:00-23:20 add solution by constructive approach
+ 
+ 2xW:
+ o---->
+ -->o->
+ 
+ 3xW:
+ o---->
+ ->o-->
+ o---->
+ 
+ 4x4:
+ o-->
+ ->o>
+ o-->
+ ->o>
+ (swap is needed to avoid diagonal conflict)
+
+ https://codingcompetitions.withgoogle.com/codejam/round/0000000000051635/0000000000104e03
+  - constructive
+  - randomization
+  - greedy
+ http://drken1215.hatenablog.com/entry/2019/04/13/143500
+  - constructive
+ http://kmjp.hatenablog.jp/entry/2019/04/15/0930
+  - constructive
+ https://twitter.com/xuzijian629/status/1116920887016599552
+  - constructive
+ https://twitter.com/satanic0258/status/1116907238172397569
+  - constructive
+ https://twitter.com/yosupot/status/1116933713680461824
+  - randomization
+ https://shashankmishracoder.wordpress.com/2019/04/13/google-code-jam-2019-round-1a-pylon-dfs-and-backtracking-approach/
+  - randomization
  
  */
 
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
 int R,C;
+
+void nope() {
+  cout<<"IMPOSSIBLE"<<endl;
+}
+vector<II> f(int H, int W, int off) {
+  VI P(H,0); REP(i,H) if(i%2==1) P[i]=off;
+  vector<II> res;
+  int i=0;
+  set<II> S;
+  while(SZ(res)<H*W) {
+    res.emplace_back(i,P[i]);
+    S.emplace(i,P[i]);
+    P[i]=(P[i]+1)%W;
+    i=(i+1)%H;
+  }
+  assert(SZ(S)==H*W);
+  return res;
+}
+void show(vector<vector<II>> res, bool swapped) {
+//  dump(SZ(res));
+  cout<<"POSSIBLE"<<endl;
+  int roff=0;
+  set<II> S;
+  FORR(X,res) {
+//    dump(SZ(X));
+    FORR(p,X) {
+      int i=p.first+1+roff,j=p.second+1;
+      if(swapped) swap(i,j);
+      cout<<i<<" "<<j<<endl;
+      S.emplace(i,j);
+    }
+    int H=SZ(X)/C;
+    roff+=H;
+  }
+//  if(SZ(S)!=R*C) dump(SZ(S),R*C);
+  assert(SZ(S)==R*C);
+}
+void solve() {
+//  dump(R,C);
+  bool swapped=false;
+  if(R>C) swap(R,C),swapped=true;
+  if(R==1) {
+    nope();
+    return;
+  }
+  if(R==2&&C<5) {
+    nope();
+    return;
+  }
+  if(R==3&&C<4) {
+    nope();
+    return;
+  }
+  vector<vector<II>> res;
+  if(R==4&&C==4) {
+    vector<II> X=f(R,C,2);
+    int M=SZ(X);
+    swap(X[M-4],X[M-3]),swap(X[M-2],X[M-1]);
+    res.push_back(X);
+  } else {
+    if(R%2==0) REP(_,R/2) res.push_back(f(2,C,3));
+    else {
+      REP(_,(R-3)/2) res.push_back(f(2,C,3));
+      res.push_back(f(3,C,2));
+    }
+  }
+  show(res,swapped);
+}
+
 VI G[400];
+int viz[400];
 int ith(int mask, int i) {
   return (mask>>i)&1;
 }
-int f(int p, int mask, VI &path) {
-  int full=(1<<(R*C))-1;
-  if(mask==full) {
+int f(int p, int cnt, VI &path) {
+  if(cnt==R*C) {
     cout<<"POSSIBLE"<<endl;
     assert(SZ(path)==R*C);
     REP(k,SZ(path)) {
@@ -69,18 +182,19 @@ int f(int p, int mask, VI &path) {
     }
     return 1;
   }
-  dump(p,mask,__builtin_popcount(mask));
-  dumpc(G[p]);
-  FORR(q,G[p]) if(!ith(mask,q)) {
+//  shuffle(G[p].begin(), G[p].end(), rnd);
+  FORR(q,G[p]) if(!viz[q]) {
     path.push_back(q);
     dump(p,q);
-    int res=f(q,mask|(1<<q),path);
+    viz[q]=1;
+    int res=f(q,cnt+1,path);
+    viz[q]=0;
     path.pop_back();
     if(res) return 1;
   }
   return 0;
 }
-void solve() {
+void solve_bruteforce() {
   dump(R,C);
   REP(i,400) G[i].clear();
   REP(i1,R)REP(j1,C) {
@@ -89,39 +203,26 @@ void solve() {
       if(i1+j1==i2+j2) continue;
       int u=i1*C+j1,v=i2*C+j2;
       G[u].push_back(v);
-//      cerr<<u<<" "<<v<<endl;
     }
   }
+  REP(i,R*C) shuffle(G[i].begin(), G[i].end(), rnd);
   REP(i,R)REP(j,C) {
     int u=i*C+j;
     VI p={u};
-    int res=f(u,1<<(u),p);
+    ZERO(viz);
+    viz[u]=1;
+    int res=f(u,1,p);
     if(res) return;
   }
-/*  int full=(1<<(R*C))-1;
-  MINUS(memo),MINUS(path);
-  REP(i,R)REP(j,C) {
-    int res=f(i*C+j,1<<(i*C+j));
-    if(res) {
-      cout<<"POSSIBLE"<<endl;
-      VI ans;
-      int p=i*C+j,mask=1<<(i*C+j);
-      while(mask!=full) {
-        int q=path[p][mask];
-        ans.push_back(q);
-        p=q,mask|=(1<<q);
-      }
-      assert(SZ(ans)==R*C);
-      REP(k,SZ(ans)) {
-        int i=ans[k]/C,j=ans[k]%C;
-        cout<<i+1<<" "<<j+1<<endl;
-      }
-      return;
-    }
-  }*/
   cout<<"IMPOSSIBLE"<<endl;
 }
 int main(int argc, char* argv[]) {
+  /*
+  cout<<19*19<<endl;
+  FORE(i,2,20)FORE(j,2,20) {
+    cout<<i<<" "<<j<<endl;
+  }*/
+  
   int T; cin>>T;
   for(int t=1; t<=T; ++t) {
     cin>>R>>C;
