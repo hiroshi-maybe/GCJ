5cad58726c23300f89fe132dbf0cfc63141e784d
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-04-19 13:51:51 -0700

    solve KS2020 RB C. Robot Path Decoding

---
+++ kickstart/2020B/RobotPathDecoding.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+const int MOD=1000000000;
+//const int MOD=998244353;
+struct ModInt {
+  unsigned int val;
+  ModInt(): val(0) {}
+  ModInt(int v) { norm(v%MOD); }
+  ModInt(long long v) { norm(v%MOD); }
+  ModInt& norm(long long v) {
+    v=v<0?v%MOD+MOD:v; // negative
+    v=v>=MOD?v-MOD:v; // mod
+    val=(unsigned int)v;
+    return *this;
+  }
+  explicit operator bool() const { return val!=0; }
+  ModInt operator-() const { return ModInt(0)-*this; }
+  ModInt &operator+=(ModInt that) { return norm((long long)val+that.val); }
+  ModInt &operator-=(ModInt that) { return norm((long long)val-that.val); }
+  ModInt &operator*=(ModInt that) { val=(unsigned long long)val*that.val%MOD; return *this; }
+  ModInt &operator/=(ModInt that) { return *this*=that.inv(); }
+  ModInt operator+(ModInt that) const { return ModInt(*this)+=that; }
+  ModInt operator-(ModInt that) const { return ModInt(*this)-=that; }
+  ModInt operator*(ModInt that) const { return ModInt(*this)*=that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this)/=that; }
+  ModInt pow(long long n) const {
+    ModInt x=*this, res=1;
+    while(n>0) {
+      if(n&1) res*=x;
+      x*=x,n>>=1;
+    }
+    return res;
+  }
+  ModInt inv() const { return (*this).pow(MOD-2); }
+  bool operator==(ModInt that) const { return val==that.val; }
+  bool operator!=(ModInt that) const { return val!=that.val; }
+  friend ostream& operator<<(ostream& os, const ModInt& that) { return os<<that.val; }
+};
+// $ cp-batch RobotPathDecoding | diff RobotPathDecoding.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address RobotPathDecoding.cpp && ./a.out
+
+/*
+ 
+ 4/18/2020
+ 
+ 16:17-16:47 AC
+ 
+ https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ffc8/00000000002d83dc
+ 
+ */
+
+string S;
+
+map<char,pair<ModInt,ModInt>> moves = { {'E',{0,1}}/*R*/,{'S',{1,0}}/*D*/,{'W',{0,-1}}/*L*/,{'N',{-1,0}}/*U*/ };
+
+pair<ModInt,ModInt> f(int &p) {
+  int N=SZ(S);
+  pair<ModInt,ModInt> res={0,0};
+  while(p<N&&(moves.count(S[p])||isdigit(S[p]))) {
+    if(isdigit(S[p])) {
+      auto rep=ModInt(S[p]-'0');
+      ++p;
+      assert(S[p]=='(');
+      auto ans=f(++p);
+      assert(S[p]==')');
+      ++p;
+      res.first+=ans.first*rep,res.second+=ans.second*rep;
+    } else {
+      char c=S[p];
+      assert(moves.count(c));
+      res.first+=moves[c].first,res.second+=moves[c].second;
+      ++p;
+    }
+  }
+  if(p<N) assert(S[p]==')');
+  return res;
+}
+void solve() {
+  int p=0;
+  auto res=f(p);
+  LL a=res.second.val,b=res.first.val;
+  cout<<a+1<<" "<<b+1<<endl;
+}
+
+int main(int argc, char* argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  int T; cin>>T;
+  for(int t=1; t<=T; ++t) {
+    cin>>S;
+    
+    cout<<"Case #"<<t<<": ";
+    solve();
+  }
+  return 0;
+}
