262d21104b7208d591e30aa56e03b22e3a7979e8
Hiroshi Kori <hiroshi.maybe@gmail.com>
2022-04-02 23:03:20 -0700

    solve GCJ2022 QR B. 3D Printing

---
+++ 2022-QR/3d_printing.rs
@@ -0,0 +1,131 @@
+#![allow(unused_macros, unused_imports)]
+use std::cmp::*;
+use std::collections::*;
+
+use crate::cplib::io::*;
+use crate::cplib::minmax::*;
+use crate::cplib::vec::*;
+// region: cplib
+#[rustfmt::skip]
+#[allow(dead_code)]
+#[macro_use]
+pub mod cplib {
+#[macro_use]
+pub mod io {
+	macro_rules! _with_dollar_sign { ($($body:tt)*) => { macro_rules! __with_dollar_sign { $($body)* } __with_dollar_sign!($); }}
+	macro_rules! setup_out {
+		($fn:ident,$fn_s:ident) => {
+			use std::io::Write;
+			let out = std::io::stdout();
+			let mut out = ::std::io::BufWriter::new(out.lock());
+				_with_dollar_sign! { ($d:tt) => {
+				macro_rules! $fn { ($d($format:tt)*) => { let _ = write!(out,$d($format)*); } }
+				macro_rules! $fn_s { ($d($format:tt)*) => { let _ = writeln!(out,$d($format)*); } }
+			}}
+		};
+	}
+	macro_rules! _epr { ($v:expr $(,)?) => {eprint!("{} = {:?}, ", stringify!($v), $v)}; }
+	macro_rules! dbgln { ($($val:expr),*) => {{ eprint!("[{}:{}] ", file!(), line!()); ($(_epr!($val)),*); eprintln!(); }}; }
+	pub fn readln_str() -> String {
+		let mut line = String::new();
+		::std::io::stdin().read_line(&mut line).unwrap_or_else(|e| panic!("{}", e));
+		line
+	}
+	macro_rules! _read {
+		($it:ident; [char]) => { _read!($it; String).chars().collect::<Vec<_>>() };
+		($it:ident; [u8]) => { Vec::from(_read!($it; String).into_bytes()) };
+		($it:ident; usize1) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1 };
+		($it:ident; [usize1]) => { $it.map(|s| s.parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1).collect::<Vec<_>>() };
+		($it:ident; [$t:ty]) => { $it.map(|s| s.parse::<$t>().unwrap_or_else(|e| panic!("{}", e))).collect::<Vec<_>>() };
+		($it:ident; $t:ty) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<$t>().unwrap_or_else(|e| panic!("{}", e)) };
+		($it:ident; $($t:tt),+) => { ($(_read!($it; $t)),*) };
+	}
+	macro_rules! readlns {
+		($($t:tt),*; $n:expr) => {{ let stdin = ::std::io::stdin();
+			::std::io::BufRead::lines(stdin.lock()).take($n).map(|line| {
+				let line = line.unwrap(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*)
+			}).collect::<Vec<_>>()
+		}};
+	}
+	macro_rules! readln {
+		($($t:tt),*) => {{ let line = cplib::io::readln_str(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*) }};
+	}
+}
+#[macro_use]
+pub mod minmax {
+	pub trait SetMinMax {
+		fn setmin(&mut self, other: Self) -> bool;
+		fn setmax(&mut self, other: Self) -> bool;
+	}
+	macro_rules! _set { ($self:ident, $cmp:tt, $other:ident) => {{
+			let update = $other $cmp *$self;
+			if update { *$self = $other; }
+			update
+	}}; }
+	impl<T> SetMinMax for T where T: PartialOrd {
+		fn setmin(&mut self, other: T) -> bool { _set!(self, <, other) }
+		fn setmax(&mut self, other: T) -> bool { _set!(self, >, other) }
+	}
+}
+#[macro_use]
+pub mod vec {
+	pub trait CollectVec: Iterator { fn collect_vec(self) -> Vec<Self::Item> where Self: Sized { self.collect() } }
+	impl<T> CollectVec for T where T: Iterator {}
+	macro_rules! vvec {
+		($v:expr; $n:expr) => { Vec::from(vec![$v; $n]) };
+		($v:expr; $n:expr $(; $ns:expr)+) => { Vec::from(vec![vvec![$v $(; $ns)*]; $n]) };
+	}
+}
+}
+// endregion: cplib
+
+// $ cp-batch 3d_printing | diff 3d_printing.out -
+// $ cargo run --bin 3d_printing
+
+///
+/// 4/1/2022
+///
+/// 22:48-23:11 AC
+///
+/// https://codingcompetitions.withgoogle.com/codejam/round/0000000000876ff1/0000000000a4672b#analysis
+/// https://kmjp.hatenablog.jp/entry/2022/04/03/0900
+///
+
+fn solve() -> Option<Vec<usize>> {
+    let inf = 1_000_000;
+    let mut xs = vec![inf; 4];
+    for _ in 0..3 {
+        let ys = readln!([usize]);
+        for i in 0..4 {
+            xs[i].setmin(ys[i]);
+        }
+    }
+
+    if xs.iter().sum::<usize>() < inf {
+        None
+    } else {
+        let mut rem = inf;
+        let mut res = vec![];
+        for i in 0..4 {
+            let x = min(xs[i], rem);
+            rem -= x;
+            res.push(x);
+        }
+        Some(res)
+    }
+}
+
+fn main() {
+    setup_out!(put, puts);
+
+    for t in 0..readln!(usize) {
+        let res = solve();
+        put!("Case #{}: ", t + 1);
+        if let Some(xs) = res {
+            assert_eq!(xs.iter().sum::<usize>(), 1_000_000);
+            puts!("{} {} {} {}", xs[0], xs[1], xs[2], xs[3]);
+        } else {
+            puts!("IMPOSSIBLE");
+        }
+    }
+}
